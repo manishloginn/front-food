58af802d891f18c5d6a9f59c3cac928d499aeee5
manishloginn <manishv7714@gmail.com>
2024-08-25 00:39:11 +0530

    done

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -59,7 +59,7 @@ function Home() {
                 </div>
                 <div className='lowersection' key={item.name}>
                   <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                    <img src={`https://foodworld-nine.vercel.app/${item.image}`}></img>
+                    <img src={`${apiUrl}/${item.image}`}></img>
                     <button onClick={() => haldelcart(item)} style={{ marginTop: "-20px", border: "1px solid grey", width: "100px", background: "white", color: "green", fontWeight: "800", fontSize: "20px" }}>ADD</button>
                   </div>
 
